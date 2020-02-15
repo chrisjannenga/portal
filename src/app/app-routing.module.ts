1d6a570026f0c525dfd32eb119226ec22651ccc5
Chris Jannenga <cjannenga312@gmail.com>
2020-02-14 23:40:32 -0700

    Added Auth0 and created service. Missing Auth Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
-  { path: 'services', loadChildren: () => import('./pages/services/services.module').then(m => m.ServicesModule) }
+  { path: 'services', loadChildren: () => import('./pages/services/services.module').then(m => m.ServicesModule) },
+  { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) }
 ];
 
 @NgModule({
